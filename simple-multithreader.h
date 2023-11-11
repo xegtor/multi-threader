e1203ac2fb7c92eecae852723ea07fda7e9f0057
Xegtor <rohakkansal04@gmail.com>
2023-11-11 22:19:18 +0530

    starting writing the parallel_for function

--- simple-multithreader.h
+++ simple-multithreader.h
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <cstring>
 
+void parallel_for(int low, int high, std::function<void(int)> &&lambda, int numThreads);
+void parallel_for(int low1, int high1, int low2, int high2, std::function<void(int, int)> &&lambda, int numThreads);
+
 int user_main(int argc, char **argv);
 
 /* Demonstration on how to pass lambda as parameter.
@@ -45,4 +48,35 @@ int main(int argc, char **argv) {
 
 #define main user_main
 
+void parallel_for(int low, int high, std::function<void(int)> &&lambda, int numThreads) {
+    auto start_time = std::chrono::high_resolution_clock::now();  // Record start time
 
+    int range = high - low + 1;
+    int chunk_size = range / numThreads;
+    std::vector<pthread_t> threads(numThreads);
+
+    for (int i = 0; i < numThreads; ++i) {
+        int thread_low = low + i * chunk_size;
+        int thread_high = (i == numThreads - 1) ? high : thread_low + chunk_size - 1;
+
+        auto data = new std::pair<std::function<void(int)>, int>(lambda, thread_low);
+
+        if (pthread_create(&threads[i], nullptr, parallel_for_helper, data) != 0) {
+            std::cerr << "Error creating thread " << i << std::endl;
+            return;
+        }
+    }
+
+    for (int i = 0; i < numThreads; ++i) {
+        pthread_join(threads[i], nullptr);
+    }
+
+    auto end_time = std::chrono::high_resolution_clock::now();  // Record end time
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+
+    std::cout << "Total execution time: " << duration.count() << " microseconds" << std::endl;
+}
+
+void parallel_for(int low1, int high1, int low2, int high2, std::function<void(int, int)> &&lambda, int numThreads){
+
+}
